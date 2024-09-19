38e7d27620566eced8e17596d2fca7f6bd5d0c6e
Kassidy Dreher <kassidy@Kassidys-MacBook-Air.local>
2024-09-19 15:43:40 -0700

    initialize stables and add a horse

---
+++ stables.js
@@ -0,0 +1,12 @@
+console.log("horse!")
+let horseName = "Coolguy"; // this is a commemt or a note
+horseName = "Lulu";
+let horseAge = 4;
+console.log(horseName);
+console.log(horseAge);
+let isHorseInStable = false;
+console.log(isHorseInStable);
+console.log(horseName);
+horseName = "Fancy Lulu"
+console.log(horseName);
+
