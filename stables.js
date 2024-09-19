bb8c046253ca849b79780d663e851c35ab05130e
Kassidy Dreher <kassidy@Kassidys-MacBook-Air.local>
2024-09-19 16:27:40 -0700

    Stables revision

--- stables.js
+++ stables.js
@@ -1,12 +1,8 @@
 console.log("horse!")
-let horseName = "Coolguy"; // this is a commemt or a note
-horseName = "Lulu";
+let horseName = "Fancy Lulu"; // this is a commemt or a note
 let horseAge = 4;
-console.log(horseName);
 console.log(horseAge);
 let isHorseInStable = false;
-console.log(isHorseInStable);
-console.log(horseName);
-horseName = "Fancy Lulu"
-console.log(horseName);
+console.log(isHorseInStable)
+
 
