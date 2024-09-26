191008ea9eacdd14c2570a3ebb651f424d9b3278
Kassidy Dreher <kassidy@Kassidys-MacBook-Air.local>
2024-09-26 14:38:48 -0700

    add operators to my stable

--- stables.js
+++ stables.js
@@ -1,8 +1,21 @@
 console.log("horse!")
 let horseName = "Fancy Lulu"; // this is a commemt or a note
 let horseAge = 4;
+console.log(horseName);
 console.log(horseAge);
 let isHorseInStable = false;
 console.log(isHorseInStable)
 
+const STABLE_MONTHLY_FEE = 100;
+let horseIntroduction = " is the name of my horse"
+// let rentIntroduction = "it costs " + STABLE_MONTHLY_FEE + " to board " + horseName;
+let rentIntroduction = `It costs ${STABLE_MONTHLY_FEE} to board ${horseName}`; 
+let stableIntroduction = `Welcome to "${horseName}'s" stable`;
+console.log(stableIntroduction);
+console.log(rentIntroduction);
+
+
+
+console.log(STABLE_MONTHLY_FEE);
+console.log(STABLE_MONTHLY_FEE * 5);
 
